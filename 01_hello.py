cf69836e1c633302ce3ee4d7eedb56f33562f826
Frederick <frederickvancleve@hotmail.com>
2026-06-14 18:34:38 -0400

    Add first Python script with variables and comments

---
+++ 01_hello.py
@@ -0,0 +1,12 @@
+print("Hello, world!")
+
+# This is a comment. Python ignores anything after the # on a line.
+# Comments are for humans (you and future you).
+
+name = "Frederick"          # A variable holding text (a string)
+age = 46               # A variable holding a whole number (an integer)
+height_m = 1.8542        # A variable holding a decimal number (a float)
+
+print("My name is", name)
+print("I am", age, "years old")
+print("My height is", height_m, "meters")
