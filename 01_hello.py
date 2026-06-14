b05d1f0a78265dafd3fa767d8aa15a731548e972
Frederick <frederickvancleve@hotmail.com>
2026-06-14 19:03:39 -0400

    additional commit using if else

--- 01_hello.py
+++ 01_hello.py
@@ -3,15 +3,21 @@ print("Hello, world!")
 # This is a comment. Python ignores anything after the # on a line.
 # Comments are for humans (you and future you).
 
-name = "Frederick"          # A variable holding text (a string)
-age = 46               # A variable holding a whole number (an integer)
-height_m = 1.8542        # A variable holding a decimal number (a float)
-major = "Machine Learning, A.I." # A variable holding text (a string)
-job_title = "Network Engineer" # A variable holding text (a string)
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+height_m = float(input("What is your height in meters? "))
+major = input("What is your major? ")
+job_title = input("What is your job title? ")
 
+print(f"My name is {name}")
+print(f"I am {age} years old")
+print(f"My height is {height_m:.2f} meters")
+print(f"My major is {major}")
+print(f"My job title is {job_title}")
 
-print("My name is", name)
-print("I am", age, "years old")
-print("My height is", height_m, "meters")
-print("My major is", major)
-print("My job title is", job_title)
+print()
+
+if age > 40:
+    print("You've got a ton of real-world experience!")
+else:
+    print("You're building a strong foundation early on!")
