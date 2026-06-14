1f45578b130eb766c0a8262916763b410af505cf
Frederick <frederickvancleve@hotmail.com>
2026-06-14 18:54:53 -0400

    New comment

--- 01_hello.py
+++ 01_hello.py
@@ -6,7 +6,12 @@ print("Hello, world!")
 name = "Frederick"          # A variable holding text (a string)
 age = 46               # A variable holding a whole number (an integer)
 height_m = 1.8542        # A variable holding a decimal number (a float)
+major = "Machine Learning, A.I." # A variable holding text (a string)
+job_title = "Network Engineer" # A variable holding text (a string)
+
 
 print("My name is", name)
 print("I am", age, "years old")
 print("My height is", height_m, "meters")
+print("My major is", major)
+print("My job title is", job_title)
